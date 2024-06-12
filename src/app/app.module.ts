4645009c7e98dcc5896f2d197685c1dc13d90b30
david5garcia <davidgballester@gmail.com>
2024-06-13 00:15:02 +0200

    crear componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PokemonComponent } from './components/pokemon/pokemon.component';
+import { PokemonsComponent } from './components/pokemons/pokemons.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PokemonComponent,
+    PokemonsComponent
   ],
   imports: [
     BrowserModule,
